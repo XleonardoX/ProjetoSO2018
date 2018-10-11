ca437b2a3324db840bb9981fb8dc6c8e6c489391
XleonardoX <42012359+XleonardoX@users.noreply.github.com>
2018-10-11 13:03:14 -0300

    Update multicat.c

--- multicat.c
+++ multicat.c
@@ -9,15 +9,15 @@
 
 
 typedef struct Multicat_integer{
-        int *v;
+        int *v;                   //vetor de inteiros encontrados nos arquivos de entrada
         int top;                  //referente ao tamanho do vetor v
-        int argumentos;
-        char arquivo[100][20];  //variavel que guarda o nomes de arquivos de entrada e saída
+        int argumentos;           //numero de argumentos passados na linha de comando
+        char arquivo[100][20];    //variavel que guarda o nomes de arquivos de entrada e saída
 }Multicat;
 
 
-
-void *Multicat_integer_sort(void *z);
+void *Multicat_integer(Multicat *z);
+void *Ordena(void *p);
 void Salva_Arq_Out(Multicat *w);
 
 //===========================================================================================================================
@@ -34,29 +34,30 @@ int main(int argc, char *argv[]){
             printf("Comando insuficiente para execução do programa\n");     //"./multicat 16 arq1.in arqSaida.out"
             return 0;                                                    
          }
-         
+
          Num_de_threads = atoi(argv[1]);        //argumento 1 referente ao numero de threads
          pthread_t threads[Num_de_threads];     //vetor de threads
          
-         for(i=0; i<((x->argumentos)-2); i++){
+         for(i=0; i<((x->argumentos)-2); i++){  //copia os nomes dos arquivos de entrada e do arquivo resultante num vetor de strings
               strcpy(x->arquivo[i],argv[i+2]);
          }
+         Multicat_integer(x);
          
-         clock_t start_time;    //para medir o tempo de execução
+         clock_t start_time;    //para medir o tempo de processamento usando threads
          start_time = clock(); 
 
          for(i=0; i<(Num_de_threads); i++){                                                 
-            pthread_create(&threads[i],NULL,Multicat_integer_sort,(void *)x);    //metade das threads para a função multicat
+            pthread_create(&threads[i],NULL,Ordena,(void *)x);    //criação das threads para ordenação
          }
          
          for (i=0;i<Num_de_threads;i++){
               pthread_join(threads[i],NULL); //A funcao bloqueia o processo ate que o thread indicado termine
          }
          
-         double time_SEC = (clock() - start_time) / (double)CLOCKS_PER_SEC;  //tempo total de execução
+         double time_SEC = (clock() - start_time) / (double)CLOCKS_PER_SEC;  //tempo de processamento usando threads
 
          Salva_Arq_Out(x);
-         printf("Tempo de execução usando %d threads: %f segundos.\n", Num_de_threads, time_SEC);
+         printf("Tempo de processameto usando %d threads: %f segundos.\n", Num_de_threads, time_SEC);
          
          free(x);
          pthread_exit(NULL);
@@ -66,38 +67,44 @@ int main(int argc, char *argv[]){
 
 //==========================================================================================================================
 
-void *Multicat_integer_sort(void *z){  //le os arquivos e grava os inteiros num vetor desordenado
+void *Multicat_integer(Multicat *z){  //le os arquivos e grava os inteiros num vetor desordenado
      
          FILE *f;
          int aux, i;
          int j=0;
-         ((Multicat *)z)->top = 0;
+         z->top = 0;
 
-         for(int i=0; i<(((Multicat *)z)->argumentos)-3; i++){        //desconsidera os comandos "./multicat" "Num threads" "arqSaida.out"
-              f = fopen((((Multicat *)z)->arquivo[i]),"r");           //EOF representa o fim do arquivo de entrada
-              while(fscanf(f,"%d\n",&aux)!=EOF){                      //para saber o tamanho do vetor que deve ser alocado           
-                    ((Multicat *)z)->top++;                           //apos passar por todos arquivos de entrada
+         for(int i=0; i<(z->argumentos)-3; i++){        //desconsidera os comandos "./multicat" "Num threads" "arqSaida.out"
+              f = fopen((z->arquivo[i]),"r");           //EOF representa o fim do arquivo de entrada
+              while(fscanf(f,"%d\n",&aux)!=EOF){        //para saber o tamanho do vetor que deve ser alocado           
+                    z->top++;                           //apos passar por todos arquivos de entrada
               }
               fclose(f);
          } 
 
-         ((Multicat *)z)->v= (int*) malloc(((Multicat *)z)->top*sizeof(int));       //alocação do vetor resultante
+         z->v= (int*) malloc(z->top*sizeof(int));       //alocação do vetor resultante
 
-         for(int i=0; i<(((Multicat *)z)->argumentos)-3; i++){        //desconsidera os comandos "./multicat" "Num threads" "arqSaida.out"
-              f = fopen((((Multicat *)z)->arquivo[i]),"r");
-              while(fscanf(f,"%d\n",&aux)!=EOF){                   
-                   ((Multicat *)z)->v[j] = aux;
+         for(int i=0; i<(z->argumentos)-3; i++){        //desconsidera os comandos "./multicat" "Num threads" "arqSaida.out"
+              f = fopen((z->arquivo[i]),"r");
+              while(fscanf(f,"%d\n",&aux)!=EOF){        //atribuição dos inteiros dos arquivos no vetor          
+                   z->v[j] = aux;                       //vetor ainda desordenado
                    j++;
               }
             fclose(f);
          } 
+}
 
-         for(j=(((Multicat *)z)->top-1); j>0; j--){      //Ordenação Bublle Sort
+//================================================================================================================================
+
+void *Ordena(void *p){
+         int aux, i, j;
+
+         for(j=(((Multicat *)p)->top-1); j>0; j--){      //Ordenação Bublle Sort
               for(i=0; i<j; i++){
-                 if(((Multicat *)z)->v[i] > ((Multicat *)z)->v[i+1]){
-                    aux = ((Multicat *)z)->v[i];
-                    ((Multicat *)z)->v[i] = ((Multicat *)z)->v[i+1];
-                    ((Multicat *)z)->v[i+1] = aux;
+                 if(((Multicat *)p)->v[i] > ((Multicat *)p)->v[i+1]){
+                    aux = ((Multicat *)p)->v[i];
+                    ((Multicat *)p)->v[i] = ((Multicat *)p)->v[i+1];
+                    ((Multicat *)p)->v[i+1] = aux;
                  }
               }
          }
@@ -106,7 +113,7 @@ void *Multicat_integer_sort(void *z){  //le os arquivos e grava os inteiros num 
 
 //===============================================================================================================================
 
-void Salva_Arq_Out(Multicat *w){   //função que salva os vetor ordenado num arquivo de saida
+void Salva_Arq_Out(Multicat *w){   //função que salva o vetor ordenado num arquivo de saida
 
          FILE *f;
          f = fopen((w->arquivo[(w->argumentos)-3]), "w+");  //referente ao arquivo "arqSaida.out"
